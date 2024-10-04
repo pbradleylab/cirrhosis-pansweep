4c38f86d3f3f3ed3c98b03aca5aed28c9ef5bf3c
Patrick Bradley <bradley.720@osu.edu>
2024-10-04 17:51:22 -0400

    tidying

---
+++ analyses/make_cirr_metadata.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readxl)
+cirr_csv <- read_csv("../data/manual/SraRunTable.txt")
+cirr_md <- cirr_csv %>%
+  mutate(env = ifelse(grepl("^H", `Sample_Name`), "control", "case")) %>%
+  separate_wider_delim(`Sample_Name`, delim="_", names=c("subject", "runnum")) %>%
+  select(sample=Run, subject, env) %>% mutate(subject=gsub("-","",subject))
+write_tsv(cirr_md, "../data/manual/cirrhosis-metadata.tsv")
+
